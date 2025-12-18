cd332bba9b0eddef47a07003641e2968e5007b05
Om-Shri <som667075@gmail.com>
2025-12-18 08:50:07 +0530

    some extra code

--- Generator.py
+++ Generator.py
@@ -2,5 +2,10 @@ def reverse(data):
     for index in range(len(data)-1,-1,-1):
         yield data[index]
 
-for i in reverse("Rajesh Kumar"):
-    print(i)
+# for i in reverse("Rajesh Kumar"):
+#     print(i)
+name = "Subodh narayan"
+var = iter(name)
+
+for i in range(1,len(name),1):
+    print(next(var))
