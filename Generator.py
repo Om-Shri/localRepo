b0b292ebb50ecae50d9cde12fb6ed6b97d4846a0
Om-Shri <som667075@gmail.com>
2025-12-19 08:38:15 +0530

    Some extra prectice

--- Generator.py
+++ Generator.py
@@ -9,3 +9,13 @@ var = iter(name)
 
 for i in range(1,len(name),1):
     print(next(var))
+
+print(sum(i*i for i in range(10)) )                # sum of squares
+
+
+xvec = [10, 20, 30]
+yvec = [7, 5, 3]
+print(sum(x*y for x,y in zip(xvec, yvec)))         # dot product
+
+data = 'golf'
+list(data[i] for i in range(len(data)-1, -1, -1))
