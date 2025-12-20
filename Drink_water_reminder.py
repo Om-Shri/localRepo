c85107e5f5b1099f3ca4e98ff3876dffcefe6894
Om-Shri <som667075@gmail.com>
2025-12-20 08:07:22 +0530

    Some improvments

--- Drink_water_reminder.py
+++ Drink_water_reminder.py
@@ -1,24 +1,15 @@
 ## Drink water reminder
-## ALL OPERATING SYSTEM.
+##  WINDOWS SYSTEM.
 import time
 from plyer import notification
+a = input("Enter hours for notification (1,2,3,4...) :")
+
+sedule = int(a)*3600
+
 while True:
     notification.notify(
         title="Reminder",
         message="Drink one glass of water.You are dehydrated.",
     )
-    time.sleep(3600)
+    time.sleep(sedule)
 
-## ONLY FOR WINDOWS.BUT THIS IS PROPERLY WORK IN VERSON 3.10 3.11 OR LOWER VERSONS.
-
-from win10toast import ToastNotifier
-
-toast = ToastNotifier()
-
-while True:
-    toast.show_toast(
-        "Water Reminder",
-        "Drink one glass of water",
-        duration=5
-    )
-    time .sleep(3600)
